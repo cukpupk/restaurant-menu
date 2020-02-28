367dabd48b53139ef31de82dd3aa0e3f02ad9f30
tangshiw <cukpuk@gmail.com>
2020-02-27 22:12:44 -0800

    restaurant push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Categories from "./Categories";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>{"1. Welcome to Chef Chu's Restaurant"}</h3>
+        <Router>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/Categories">
+            <button>Categories</button>
+          </Link>
+
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/Categories" component={Categories} exact />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
